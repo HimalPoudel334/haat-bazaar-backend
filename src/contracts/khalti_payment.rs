5544f1f3844c57e6dacf04487250ab6645baef57
Himal Poudel <himalp334@gmail.com>
2024-08-06 14:20:48 +0545

    changed khalti amount to paisa

--- src/contracts/khalti_payment.rs
+++ src/contracts/khalti_payment.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+/*#[derive(Debug, Deserialize, Serialize)]
 pub struct KhaltiPayment {
     pub return_url: String,
     pub website_url: String,
-    pub amount: String,
+    pub amount: i64,
     pub purchase_order_id: String,
     pub purchase_order_name: String,
     pub customer_info: CustomerInfo,
@@ -24,16 +24,124 @@ pub struct CustomerInfo {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct AmountBreakdown {
     pub label: String,
-    pub amount: String,
+    pub amount: f64,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ProductDetail {
     pub identity: String,
     pub name: String,
-    pub total_price: String,
+    pub total_price: f64,
     pub quantity: i32,
-    pub unit_price: String,
+    pub unit_price: f64,
+}*/
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CustomerInfo {
+    pub name: String,
+    pub email: String,
+    pub phone: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AmountBreakdown {
+    label: String,
+    amount: i64, // in paise
+}
+
+impl AmountBreakdown {
+    pub fn new(label: String, amount: f64) -> Self {
+        Self {
+            label,
+            amount: (amount * 100.0) as i64,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ProductDetail {
+    identity: String,
+    name: String,
+    total_price: i64, // in paise
+    quantity: i32,
+    unit_price: i64, // in paise
+}
+
+impl ProductDetail {
+    pub fn new(
+        identity: String,
+        name: String,
+        total_price: f64,
+        unit_price: f64,
+        quantity: i32,
+    ) -> Self {
+        ProductDetail {
+            identity,
+            name,
+            total_price: (total_price * 100.0) as i64,
+            unit_price: (unit_price * 100.0) as i64,
+            quantity,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct KhaltiPayment {
+    return_url: String,
+    website_url: String,
+    amount: i64, // in paise
+    purchase_order_id: String,
+    purchase_order_name: String,
+    customer_info: CustomerInfo,
+    amount_breakdown: Option<Vec<AmountBreakdown>>,
+    product_details: Option<Vec<ProductDetail>>,
+    merchant_username: String,
+    merchant_extra: String,
+}
+
+impl KhaltiPayment {
+    pub fn init(
+        return_url: String,
+        website_url: String,
+        amount: f64, // in rupees
+        purchase_order_id: String,
+        purchase_order_name: String,
+        customer_info: CustomerInfo,
+        amount_breakdown: Option<Vec<AmountBreakdown>>,
+        product_details: Option<Vec<ProductDetail>>,
+        merchant_username: String,
+        merchant_extra: String,
+    ) -> Self {
+        // let amount_breakdown = amount_breakdown.map(|breakdown| {
+        //     breakdown.into_iter().map(|ab| AmountBreakdown {
+        //         label: ab.label,
+        //         amount: (ab.amount * 100.0) as i64,
+        //     }).collect()
+        // });
+
+        // let product_details = product_details.map(|details| {
+        //     details.into_iter().map(|pd| ProductDetail {
+        //         identity: pd.identity,
+        //         name: pd.name,
+        //         total_price: (pd.total_price * 100.0) as i64,
+        //         unit_price: (pd.unit_price * 100.0) as i64,
+        //         quantity: pd.quantity,
+        //     }).collect()
+        // });
+
+        Self {
+            return_url,
+            website_url,
+            amount: (amount * 100.0) as i64,
+            purchase_order_id,
+            purchase_order_name,
+            customer_info,
+            amount_breakdown,
+            product_details,
+            merchant_username,
+            merchant_extra,
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
--- src/handlers/payment.rs
+++ src/handlers/payment.rs
@@ -375,7 +375,7 @@ pub async fn khalti_payment_get_pidx(
     println!("Hit by android");
 
     //get a pooled connection from db
-    let conn = &mut get_conn(&pool);
+    // let conn = &mut get_conn(&pool);
 
     // let customer_id: String = match utils::uuid_validator::validate_uuid(&order_json.customer_id) {
     //     Ok(c) => c,
@@ -403,52 +403,92 @@ pub async fn khalti_payment_get_pidx(
     //
 
     //Create a Khalti payment struct
-    let khalti_payment_payload: KhaltiPayment = KhaltiPayment {
-        return_url: "http://0.0.0.0:8080/payments/khalti/payment/".into(),
-        website_url: "http://0.0.0.0:8080/".into(),
-        amount: "1300".into(), // order_json.total_price, //get from the request body
-        purchase_order_id: "some id".into(), //get from request body
-        purchase_order_name: "some order name".into(),
-        customer_info: CustomerInfo {
-            name: "Sallu bhai".into(),
-            email: "Sallu@bhai.com".into(),
-            phone: "9800000001".into(),
-        },
-        amount_breakdown: Some(
-            [
-                AmountBreakdown {
-                    label: "some label".into(),
-                    amount: "1000".into(),
-                },
-                AmountBreakdown {
-                    label: "some label".into(),
-                    amount: "300".into(),
-                },
-            ]
-            .to_vec(),
-        ),
-        product_details: Some(
-            [
-                ProductDetail {
-                    identity: "some product id1".into(),
-                    name: "some product name1".into(),
-                    total_price: "500".into(),
-                    unit_price: "250".into(),
-                    quantity: 2,
-                },
-                ProductDetail {
-                    identity: "some product id2".into(),
-                    name: "some product name2".into(),
-                    total_price: "500".into(),
-                    unit_price: "100".into(),
-                    quantity: 5,
-                },
-            ]
-            .to_vec(),
-        ),
-        merchant_username: "khalti username".into(),
-        merchant_extra: String::from(""),
+    /*let khalti_payment_payload: KhaltiPayment = KhaltiPayment {
+            return_url: "http://0.0.0.0:8080/payments/khalti/payment/".into(),
+            website_url: "http://0.0.0.0:8080/".into(),
+            amount: 1300 * 100, // amount must be in paisa and should be equal to sum of amount breakdown
+            purchase_order_id: "some id".into(), //get from request body
+            purchase_order_name: "some order name".into(),
+            customer_info: CustomerInfo {
+                name: "Sallu bhai".into(),
+                email: "Sallu@bhai.com".into(),
+                phone: "9800000001".into(),
+            },
+            amount_breakdown: Some(
+                [
+                    AmountBreakdown {
+                        label: "some label".into(),
+                        amount: 1000.0.into(),
+                    },
+                    AmountBreakdown {
+                        label: "some label".into(),
+                        amount: 300.0,
+                    },
+                ]
+                .to_vec(),
+            ),
+            product_details: Some(
+                [
+                    ProductDetail {
+                        identity: "some product id1".into(),
+                        name: "some product name1".into(),
+                        total_price: 500.0,
+                        unit_price: 250.0,
+                        quantity: 2,
+                    },
+                    ProductDetail {
+                        identity: "some product id2".into(),
+                        name: "some product name2".into(),
+                        total_price: 500.0,
+                        unit_price: 100.0,
+                        quantity: 5,
+                    },
+                ]
+                .to_vec(),
+            ),
+            merchant_username: "khalti username".into(),
+            merchant_extra: String::from(""),
+        };
+    */
+
+    let customer_info = CustomerInfo {
+        name: "Sallu bhai".into(),
+        email: "Sallu@bhai.com".into(),
+        phone: "9800000001".into(),
     };
+
+    let khalti_payment_payload = KhaltiPayment::init(
+        "http://0.0.0.0:8080/payments/khalti/payment/".into(),
+        "http://0.0.0.0:8080/".into(),
+        113.0,
+        "some id".into(),
+        "some order name".into(),
+        customer_info,
+        Some(vec![
+            AmountBreakdown::new("some label".into(), 100.0),
+            AmountBreakdown::new("some label".into(), 13.0),
+        ]),
+        Some(vec![
+            ProductDetail::new(
+                "some product id1".into(),
+                "some product name1".into(),
+                50.0,
+                25.0,
+                2,
+            ),
+            ProductDetail::new(
+                "some product id2".into(),
+                "some product name2".into(),
+                50.0,
+                10.0,
+                5,
+            ),
+        ]),
+        "khalti username".into(),
+        String::from(""),
+    );
+
+    println!("khalti_payment_payload: {khalti_payment_payload:?}");
 
     let khalti_url = "https://a.khalti.com/api/v2/epayment/initiate/";
 
@@ -464,42 +504,44 @@ pub async fn khalti_payment_get_pidx(
         .send()
         .await;
 
-    let response: KhaltiResponseCamelCase =
-        match response_result {
-            Ok(res) => match res.status() {
-                reqwest::StatusCode::OK => match res.json::<KhaltiResponse>().await {
-                    Ok(r) => r.into(),
-                    Err(er) => {
-                        eprintln!("{er}");
-                        return HttpResponse::InternalServerError()
-                            .status(StatusCode::INTERNAL_SERVER_ERROR)
-                            .json(serde_json::json!({
-                                "message": format!("Error parsing response from khalti: {}", er)
-                            }));
-                    }
-                },
-                _ => match res.json::<serde_json::Value>().await {
-                    Ok(v) => {
-                        return HttpResponse::Unauthorized()
-                            .status(StatusCode::UNAUTHORIZED)
-                            .json(v)
-                    }
-                    Err(e) => return HttpResponse::InternalServerError()
+    let response: KhaltiResponseCamelCase = match response_result {
+        Ok(res) => match res.status() {
+            reqwest::StatusCode::OK => match res.json::<KhaltiResponse>().await {
+                Ok(r) => r.into(),
+                Err(er) => {
+                    eprintln!("{er}");
+                    return HttpResponse::InternalServerError()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .json(serde_json::json!({
+                            "message": format!("Error parsing response from khalti: {}", er)
+                        }));
+                }
+            },
+            _ => match res.json::<serde_json::Value>().await {
+                Ok(v) => {
+                    return HttpResponse::Unauthorized()
+                        .status(StatusCode::UNAUTHORIZED)
+                        .json(v)
+                }
+                Err(e) => {
+                    eprintln!("{e}");
+                    return HttpResponse::InternalServerError()
                         .status(StatusCode::INTERNAL_SERVER_ERROR)
                         .json(serde_json::json!({
                             "message": format!("Error parsing error response from khalti: {}", e)
-                        })),
-                },
+                        }));
+                }
             },
-            Err(e) => {
-                eprintln!("{e}");
-                return HttpResponse::InternalServerError()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .json(serde_json::json!({
-                        "message": format!("Error getting response from khalti: {}", e)
-                    }));
-            }
-        };
+        },
+        Err(e) => {
+            eprintln!("{e}");
+            return HttpResponse::InternalServerError()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .json(serde_json::json!({
+                    "message": format!("Error getting response from khalti: {}", e)
+                }));
+        }
+    };
 
     println!("----");
     println!("response: {response:?}");
